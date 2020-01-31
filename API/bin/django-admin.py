22bf11d34d07acb43080e3740536a999e9a3156b
ThomasVro <vaquerothomas@gmail.com>
2020-01-31 18:34:04 +0100

    creating Django application

---
+++ API/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Thomas/Documents/Cours ESILV/A5/Python/Projet/HAPT Data Set/API/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
